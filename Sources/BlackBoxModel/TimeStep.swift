b0357b2781b73bad9e0d87d6844de978e9a545dd
Daniel Müllenborn <damuellen@aol.com>
2020-02-21 18:05:44 +0100

    TimeStep Init updated
    Now a custom implementation is used for the calendar calculations

--- Sources/BlackBoxModel/TimeStep.swift
+++ Sources/BlackBoxModel/TimeStep.swift
@@ -28,7 +28,7 @@ public struct TimeStep: CustomStringConvertible {
     return !isDaytime
   }
   
-  public var yearDay: Int = 0
+  var year: Int = 0
   var month: Int = 0
   var day: Int = 0
   var hour: Int = 0
@@ -71,52 +71,62 @@ public struct TimeStep: CustomStringConvertible {
 
 extension TimeStep {
   
-  private static var cfCalendar: CFCalendar = {
-    let c = CFCalendarCreateWithIdentifier(
-      kCFAllocatorDefault, kCFGregorianCalendar
-    )
-    let tz = CFTimeZoneCreateWithTimeIntervalFromGMT(
-      kCFAllocatorDefault, 0
-    )
-    CFCalendarSetTimeZone(c, tz)
-    return c!
-  }()
-  
   init(_ date: Date) {
-    typealias CFCU = CFCalendarUnit
-#if os(macOS) || os(iOS)
-    let minutes = CFCU(rawValue: CFCU.minute.rawValue),
-    hours = CFCU(rawValue: CFCU.hour.rawValue),
-    days = CFCU(rawValue: CFCU.day.rawValue),
-    months = CFCU(rawValue: CFCU.month.rawValue),
-    year = CFCU(rawValue: CFCU.year.rawValue)
-#else
-    let minutes = CFCU(kCFCalendarUnitMinute),
-    hours = CFCU(kCFCalendarUnitHour),
-    days = CFCU(kCFCalendarUnitDay),
-    months = CFCU(kCFCalendarUnitMonth),
-    year = CFCU(kCFCalendarUnitYear)
-#endif
-    let refDate =  date.timeIntervalSinceReferenceDate
-    let minute = CFCalendarGetOrdinalityOfUnit(
-      TimeStep.cfCalendar, minutes, hours, refDate
-    )
-    let hour = CFCalendarGetOrdinalityOfUnit(
-      TimeStep.cfCalendar, hours, days, refDate
-    )
-    let day = CFCalendarGetOrdinalityOfUnit(
-      TimeStep.cfCalendar, days, months, refDate
-    )
-    let month = CFCalendarGetOrdinalityOfUnit(
-      TimeStep.cfCalendar, months, year,refDate
-    )
-    let yearDay = CFCalendarGetOrdinalityOfUnit(
-      TimeStep.cfCalendar, days, year, refDate
-    )
+    let ref = date.timeIntervalSinceReferenceDate
+    var absolute = Int(floor(ref / 86400.0))
+
+    func daysAfterMonth(_ month: Int, _ year: Int, _ leap: Bool) -> Int {
+      let daysAfterMonth = [365, 334, 306, 275, 245, 214, 184, 153, 122, 92, 61, 31, 0, 0, 0, 0]
+      return daysAfterMonth[month] + ((month < 2 && leap) ? 1 : 0)
+    }
     
-    self = TimeStep(
-      isDaytime: true, yearDay: yearDay,
-      month: month, day: day, hour: hour, minute: minute)
-  //  debugPrint("Current simulation time: \n\(date)")
+    func daysBeforeMonth(_ month: Int, _ year: Int, _ leap: Bool) -> Int {
+      let daysBeforeMonth = [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365, 0, 0]
+      return daysBeforeMonth[month] + ((month < 2 && leap) ? 1 : 0)
+    }
+    
+    func isleap(_ year: Int) -> Bool {
+      var y = (year + 1) % 400  /* correct to nearest multiple-of-400 year, then find the remainder */
+      if y < 0 { y = -y }
+      return (0 == (y & 3) && 100 != y && 200 != y && 300 != y);
+    }
+    
+    let b = absolute / 146097 // take care of as many multiples of 400 years as possible
+    var y = b * 400
+    var ydays = 0
+    absolute -= b * 146097;
+    while (absolute < 0) {
+      y -= 1
+      absolute += daysAfterMonth(0, y, isleap(y))
+    }
+    /* Now absolute is non-negative days to add to year */
+    ydays = daysAfterMonth(0, y, isleap(y))
+    while ydays <= absolute {
+      y += 1;
+      absolute -= ydays;
+      ydays = daysAfterMonth(0, y, isleap(y))
+    }
+
+    /* Now we have year and days-into-year */
+    let year = y
+    var m = absolute / 33 + 1 /* search from the approximation */
+    let leap = isleap(y)
+    while daysBeforeMonth(m + 1, y, leap) <= absolute { m += 1 }
+    let month = m
+    let day = absolute - daysBeforeMonth(m, y, leap) + 1
+
+    func doubleModToInt(_ d: Double, _ modulus: Int) -> Int {
+      var result = Int(floor(d - floor(d / Double(modulus)) * Double(modulus)))
+      if result < 0 { result += modulus }
+      return result
+    }
+   
+    self.isDaytime = true
+    self.year =  year + 2001
+    self.month = month
+    self.day = day
+    self.hour = doubleModToInt(floor(ref / 3600.0), 24)
+    self.minute = doubleModToInt(floor(ref / 60.0), 60)
+    self.second = doubleModToInt(ref, 60)
   }
 }
