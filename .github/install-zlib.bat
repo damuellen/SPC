618ee6a63787c855b0469368f2eff94b174c917b
Daniel Müllenborn <damuellen@aol.com>
2021-01-06 01:19:02 +0100

    Update install-zlib.bat

--- .github/install-zlib.bat
+++ .github/install-zlib.bat
@@ -1,5 +1,2 @@
-echo [ Creating dir ]
-mkdir C:\Library\Zlib
-
 echo [ Extracting zlib.zip ]
-powershell.exe -Command "Expand-Archive -Force 'C:\temp\Zlib.zip' 'C:\Library\Zlib'"
+powershell.exe -Command "Expand-Archive -Force 'Zlib.zip' 'C:\Library'"
