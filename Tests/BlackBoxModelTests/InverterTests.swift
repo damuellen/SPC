f5b87562e24497a3f75f6d3d201008b742dfafe8
Daniel Müllenborn <damuellen@aol.com>
2021-06-14 12:44:30 +0000

    Fix test

--- Tests/BlackBoxModelTests/InverterTests.swift
+++ Tests/BlackBoxModelTests/InverterTests.swift
@@ -4,7 +4,7 @@ import XCTest
 
 class InverterTests: XCTestCase {
   func testsInverter() {
-    let inverter = Inverter()
+    let inverter = PV.Inverter()
     let efficiency = inverter(power: 312359, voltage: 1148)
     XCTAssertEqual(efficiency, 97.61, accuracy: 0.01)
   }
