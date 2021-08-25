227a9d12485a41641c0dff2ba5a56c5df0d5d2b6
Daniel Müllenborn <damuellen@aol.com>
2021-08-25 16:02:10 +0000

    Regression

--- Sources/SunOl/misc.swift
+++ Sources/SunOl/misc.swift
@@ -1,4 +1,5 @@
 import Foundation
+import PhysicalQuantities
 
 precedencegroup ExponentiationPrecedence {
   associativity: right
@@ -287,5 +288,81 @@ struct Results {
       }
       print(out)
     }
+  }
+}
+
+
+extension Polynomial {
+  init(x: [Double], y: [Double], n: Int = 5) {
+    /// degree of polynomial to fit the data
+    var n: Int = n
+    /// no. of data points
+    let N: Int = min(x.count, y.count)
+
+    var X: [Double] = Array(repeating: 0.0, count: 2 * n + 1)
+
+    for i in X.indices {
+      for j in 0..<N {
+        X[i] = X[i] + pow(x[j], Double(i))  // consecutive positions of the array will store N,sigma(xi),sigma(xi^2),sigma(xi^3)....sigma(xi^2n)
+      }
+    }
+    var a: [Double] = Array(repeating: 0, count: n + 1)
+    /// B is the Normal matrix(augmented) that will store the equations, 'a' is for value of the final coefficients
+    var B: [[Double]] = Array(repeating: Array(repeating: 0, count: n + 2), count: n + 1)
+
+    for i in 0...n {
+      for j in 0...n {
+        B[i][j] = X[i + j]  // Build the Normal matrix by storing the corresponding coefficients at the right positions except the last column of the matrix
+      }
+    }
+
+    /// Array to store the values of sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
+    var Y: [Double] = Array(repeating: 0, count: n + 1)
+
+    for i in 0..<(n + 1) {
+      Y[i] = 0
+      for j in 0..<N {
+        Y[i] = Y[i] + pow(x[j], Double(i)) * y[j]  // consecutive positions will store sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
+      }
+    }
+
+    for i in 0...n {
+      B[i][n + 1] = Y[i]  // load the values of Y as the last column of B(Normal Matrix but augmented)
+    }
+
+    n = n + 1
+    for i in 0..<n {  // From now Gaussian Elimination starts(can be ignored) to solve the set of linear equations (Pivotisation)
+      for k in (i + 1)..<n {
+        if B[i][i] < B[k][i] {
+          for j in 0...n {
+            let temp = B[i][j]
+            B[i][j] = B[k][j]
+            B[k][j] = temp
+          }
+        }
+      }
+    }
+
+    for i in 0..<(n - 1) {  // loop to perform the gauss elimination
+      for k in (i + 1)..<n {
+        let t = B[k][i] / B[i][i]
+        for j in 0...n {
+          B[k][j] = B[k][j] - t * B[i][j]  // make the elements below the pivot elements equal to zero or elimnate the variables
+        }
+      }
+    }
+
+    for i in (0..<(n - 1)).reversed() {  //back-substitution
+      // x is an array whose values correspond to the values of x,y,z..
+      a[i] = B[i][n]  // make the variable to be calculated equal to the rhs of the last equation
+      for j in 0..<n {
+        if j != i {  // then subtract all the lhs values except the coefficient of the variable whose value is being calculated
+          a[i] = a[i] - B[i][j] * a[j]
+        }
+      }
+      a[i] = a[i] / B[i][i]  //now finally divide the rhs by the coefficient of the variable to be calculated
+    }
+    a.removeLast()
+    self.init(a)
   }
 }
