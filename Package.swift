f060767621059b1b5bff71deeb68fcab1387bc8a
Daniel Müllenborn <damuellen@aol.com>
2021-10-04 15:30:32 +0000

    Clean up

--- Package.swift
+++ Package.swift
@@ -2,61 +2,69 @@
 import PackageDescription
 
 let c = [CSetting.unsafeFlags(["-ffast-math", "-O3", "-fomit-frame-pointer", "-funroll-loops"])]
-let flags = [
-  "-cross-module-optimization", "-Ounchecked", "-enforce-exclusivity=unchecked", "-DRELEASE",
-]
-let linker = ["-Xlinker", "/INCREMENTAL:NO", "-Xlinker", "/IGNORE:4217,4286"]
+let flags = ["-cross-module-optimization", "-Ounchecked", "-enforce-exclusivity=unchecked",  "-DRELEASE"]
+let posix = PackageDescription.TargetDependencyCondition.when(platforms: [.linux, .macOS])
+let win = PackageDescription.BuildSettingCondition.when(platforms: [.windows])
+let linker = PackageDescription.LinkerSetting.unsafeFlags(["-Xlinker", "/INCREMENTAL:NO", "-Xlinker", "/IGNORE:4217,4286"], win)
 let swift: [SwiftSetting] = [
   .unsafeFlags(flags, .when(configuration: .release)),
   .define("DEBUG", .when(configuration: .debug)),
 ]
 var package = Package(
-  name: "SPC", platforms: [.macOS(.v10_13), .iOS(.v12)],
+  name: "SPC",
+  platforms: [.macOS(.v10_13), .iOS(.v12)],
   products: [
     .executable(name: "SolarPerformanceCalc", targets: ["SolarPerformanceCalc"]),
     .executable(name: "SolarFieldCalc", targets: ["SolarFieldCalc"]),
     .executable(name: "TransTES", targets: ["TransTES"]),
-    .executable(name: "PinchPointTool", targets: ["PinchPointTool"]),  // .library(name: "BlackBoxModel", type: .dynamic, targets: ["BlackBoxModel"]),
+    .executable(name: "PinchPointTool", targets: ["PinchPointTool"]),
+    .executable(name: "SunOl", targets: ["SunOl"]), 
+ // .library(name: "BlackBoxModel", type: .dynamic, targets: ["BlackBoxModel"]),
   ],
   dependencies: [
     .package(
-      url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.5.0")),
+      url: "https://github.com/apple/swift-argument-parser.git",
+      .upToNextMinor(from: "0.5.0")
+    ),
     .package(url: "https://github.com/damuellen/SQLite.swift.git", .branch("master")),
     .package(url: "https://github.com/damuellen/xlsxwriter.swift.git", .branch("main")),
-    // .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
     .package(
       name: "Swifter", url: "https://github.com/httpswift/swifter.git",
-      .upToNextMajor(from: "1.5.0")),// .package(url: "https://github.com/pvieito/PythonKit.git", .branch("master")),
-    // .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.1")
+      .upToNextMajor(from: "1.5.0")
+    ),  
+ // .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
+ // .package(url: "https://github.com/pvieito/PythonKit.git", .branch("master")),
+ // .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.1")
   ],
   targets: [
-    .target(name: "Libc"), .target(name: "Helpers", dependencies: ["Libc"], swiftSettings: swift),
+    .target(name: "Libc"),
     .target(
-      name: "PhysicalQuantities", dependencies: ["Config", "Meteo", "Libc", "Helpers"],
-      swiftSettings: swift),
-    .executableTarget(name: "TransTES", dependencies: ["Helpers"], swiftSettings: swift),
-    .executableTarget(
-      name: "SunOl",
-      dependencies: [
-        "Helpers", "PhysicalQuantities", .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
-        .byName(name: "Swifter", condition: .when(platforms: [.linux, .macOS])),
-      ], swiftSettings: swift, linkerSettings: [.unsafeFlags(linker, .when(platforms: [.windows]))]),
+      name: "Helpers",
+      dependencies: ["Libc"],
+      swiftSettings: swift,
+      linkerSettings: [linker]
+    ),
     .target(
-      name: "PinchPoint", dependencies: ["CPikchr", "CIAPWSIF97", "Helpers", "PhysicalQuantities"],
-      swiftSettings: swift),
-    .executableTarget(
-      name: "PinchPointTool",
-      dependencies: [
-        "PinchPoint", .product(name: "ArgumentParser", package: "swift-argument-parser"),
-        .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
-      ], swiftSettings: swift, linkerSettings: [.unsafeFlags(linker, .when(platforms: [.windows]))]),
+      name: "PhysicalQuantities",
+      dependencies: ["Config", "Meteo", "Libc", "Helpers"],
+      swiftSettings: swift
+    ), 
     .target(name: "Config", swiftSettings: swift),
-    .target(name: "DateGenerator", swiftSettings: swift), .target(name: "CPikchr", cSettings: c),
-    .target(name: "CSPA", cSettings: c), .target(name: "CSOLPOS", cSettings: c),
+    .target(name: "DateGenerator", swiftSettings: swift),
+    .target(name: "CPikchr", cSettings: c), 
+    .target(name: "CSPA", cSettings: c),
+    .target(name: "CSOLPOS", cSettings: c), 
     .target(name: "CIAPWSIF97", cSettings: c),
     .target(
-      name: "SolarPosition", dependencies: ["DateGenerator", "CSOLPOS", "CSPA"],
-      swiftSettings: swift),
+      name: "SolarPosition",
+      dependencies: ["DateGenerator", "CSOLPOS", "CSPA"],
+      swiftSettings: swift
+    ),
+    .target(
+      name: "PinchPoint",
+      dependencies: ["CPikchr", "CIAPWSIF97", "Helpers", "PhysicalQuantities"],
+      swiftSettings: swift
+    ),
     .target(
       name: "BlackBoxModel",
       dependencies: [
@@ -64,38 +72,84 @@ var package = Package(
         // .product(name: "Yams", package: "Yams"),
         .product(name: "SQLite", package: "SQLite.swift"),
         .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
-      ], swiftSettings: swift,
+      ],
+      swiftSettings: swift,
       linkerSettings: [
-        .linkedLibrary("sqlite3.lib", .when(platforms: [.windows])),
-        .unsafeFlags(linker, .when(platforms: [.windows])),
-      ]),
+        .linkedLibrary("sqlite3.lib", win), linker
+      ]
+    ),
     .target(
-      name: "ThermalStorage", dependencies: ["Libc", "Helpers", "PhysicalQuantities"],
-      swiftSettings: swift),
-    .target(name: "SolarFieldModel", dependencies: ["Libc"], swiftSettings: swift),
+      name: "ThermalStorage",
+      dependencies: ["Libc", "Helpers", "PhysicalQuantities"],
+      swiftSettings: swift
+    ), .target(name: "SolarFieldModel", dependencies: ["Libc"], swiftSettings: swift),
+    .target(
+      name: "Meteo",
+      dependencies: ["DateGenerator", "SolarPosition"],
+      swiftSettings: swift
+    ),
+    // .executableTarget(name: "Benchmarking",
+    //   dependencies: ["Meteo", "Benchmark", "BlackBoxModel"],
+    //   swiftSettings: swift),
+    
     .executableTarget(
       name: "SolarFieldCalc",
       dependencies: [
         "SolarFieldModel", "CPikchr", "Helpers",
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
         .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
-      ], swiftSettings: swift, linkerSettings: [.unsafeFlags(linker, .when(platforms: [.windows]))]),
-    .target(name: "Meteo", dependencies: ["DateGenerator", "SolarPosition"], swiftSettings: swift),
+      ],
+      swiftSettings: swift,
+      linkerSettings: [linker]
+    ),
     .executableTarget(
       name: "SolarPerformanceCalc",
       dependencies: [
         "Config", "BlackBoxModel", "Helpers",
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
         .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
-      ], swiftSettings: swift, linkerSettings: [.unsafeFlags(linker, .when(platforms: [.windows]))]),
-    // .executableTarget(name: "Benchmarking",
-    //   dependencies: ["Meteo", "Benchmark", "BlackBoxModel"],
-    //   swiftSettings: swift),
-    .testTarget(name: "MeteoTests", dependencies: ["DateGenerator", "SolarPosition", "Meteo"]),
+      ],
+      swiftSettings: swift,
+      linkerSettings: [linker]
+    ),
+    .executableTarget(
+      name: "TransTES",
+      dependencies: ["Helpers"],
+      swiftSettings: swift,
+      linkerSettings: [linker]
+    ),
+    .executableTarget(
+      name: "SunOl",
+      dependencies: [
+        "Helpers", "PhysicalQuantities",
+        .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
+        .byName(name: "Swifter", condition: posix),
+      ],
+      swiftSettings: swift,
+      linkerSettings: [linker]
+    ),
+    .executableTarget(
+      name: "PinchPointTool",
+      dependencies: [
+        "PinchPoint",
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "xlsxwriter", package: "xlsxwriter.swift"),
+      ],
+      swiftSettings: swift,
+      linkerSettings: [linker]
+    ),
+    // MARK: Tests
+    .testTarget(
+      name: "MeteoTests",
+      dependencies: ["DateGenerator", "SolarPosition", "Meteo"]
+    ), 
     .testTarget(name: "ThermalStorageTests", dependencies: ["ThermalStorage"]),
     .testTarget(name: "PinchPointTests", dependencies: ["PinchPoint"]),
     .testTarget(name: "SolarFieldModelTests", dependencies: ["SolarFieldModel"]),
     .testTarget(
       name: "BlackBoxModelTests",
-      dependencies: ["Config", "Meteo", "SolarPosition", "BlackBoxModel"]),
-  ], swiftLanguageVersions: [.v5])
+      dependencies: ["Config", "Meteo", "SolarPosition", "BlackBoxModel"]
+    ),
+  ],
+  swiftLanguageVersions: [.v5]
+)
