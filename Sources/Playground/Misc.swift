06f4067c07399c9428e43159495d3ac68a5da49d
Daniel Müllenborn <damuellen@aol.com>
2022-01-17 16:34:33 +0000

    Format

--- Sources/Playground/Misc.swift
+++ Sources/Playground/Misc.swift
@@ -8,6 +8,21 @@
 //  http://www.apache.org/licenses/LICENSE-2.0
 //
 
+
+let A = UnicodeScalar("A").value
+let columns = (0..<1100).map { n -> String in 
+  var nn = n
+  var x = ""
+  if n > 701 {
+    nn -= 676
+    x = "A"
+  }
+  let i = nn.quotientAndRemainder(dividingBy: 26)
+  let q = i.quotient > 0 ? String(UnicodeScalar(A + UInt32(i.quotient - 1))!) : ""
+  return x + q + String(UnicodeScalar(A + UInt32(i.remainder))!)
+}
+
+/*
 public struct CubicSpline {
   private var x: [Double]
   private var a: [Double]
@@ -80,6 +95,8 @@ public struct CubicSpline {
     return a[i] + af * dX + cf * (dX * dX) + df * (dX * dX * dX)
   }
 }
+
+
 import Libc
 
 extension Double {
@@ -110,3 +127,91 @@ extension Double {
     return result!
   }
 }
+
+
+func downsample(values: [(x:Double, y: Double)], threshold: Int) -> [(x:Double,y: Double)] {
+
+  guard values.count > threshold && values.count > 2 else { return values }
+  
+  let bucketSize = (values.count - 2) / (threshold - 2)
+  
+  var A = 0, nextA = 0
+  var out = [(x:Double, y: Double)]()
+  var maxAreaPoint: (x:Double, y: Double) = (x:0, y: 0)
+  out.append(values.first!)
+  
+  for i in 0..<(threshold - 2) {
+    
+    var avgRangeStart = (i + 1) * bucketSize + 1
+    var avgRangeEnd   = (i + 2) * bucketSize + 1
+    
+    avgRangeEnd = avgRangeEnd < values.count ? avgRangeEnd : values.count
+    
+    let avgRangeLength = avgRangeEnd - avgRangeStart
+    
+    var avgX = 0.0, avgY = 0.0
+    
+    while avgRangeStart < avgRangeEnd {
+      avgX += values[avgRangeStart].x
+      avgY += values[avgRangeStart].y
+      avgRangeStart += 1;
+    }
+    
+    avgX /= Double(avgRangeLength)
+    avgY /= Double(avgRangeLength)
+    
+    var rangeOffs = (i + 0) * bucketSize + 1
+    let rangeTo   = (i + 1) * bucketSize + 1
+    
+    let pointAx = values[A].x
+    let pointAy = values[A].y
+    
+    var maxArea = -1.0;
+    
+    while rangeOffs < rangeTo {
+      
+      let x = (pointAx - avgX) * ( values[rangeOffs].y - pointAy)
+      let y = (pointAx - values[rangeOffs].x ) * (avgY - pointAy)
+      let area = abs ( x - y ) * 0.5;
+      
+      if area > maxArea {
+        maxArea = area;
+        maxAreaPoint = values[rangeOffs]
+        nextA = rangeOffs
+      }
+      rangeOffs += 1
+    }
+    out.append( maxAreaPoint  )
+    A = nextA
+  }
+  out.append (values.last!)
+  return out
+}
+
+let c = CSV(atPath: "/workspaces/SPC/Saudian.csv")!
+let dni = c["DNI"]
+let ss = dni.chunks(ofCount: 24)
+var count = 1
+var result = [Double]()
+
+for day in ss {
+  let y1 = Array(day)
+  let x1 = [Double](stride(from: 0, to: Double(y1.count), by: 1))
+  let x2 = [Double](stride(from: 0, to: Double(y1.count), by: 1 / 12))
+  let d = CubicSpline(x: x1, y: y1)
+  let y2 = x2.map(d.callAsFunction(x:)).map { max(0, $0) }
+  let factor = y1.reduce(0, +) / (y2.reduce(0, +) / 12)
+  let y3 = y2.map { factor * $0 }
+  // try! Gnuplot(xs: x1, x2, x2, ys: y1, y2, y3)(.pngLarge(path: "dni_\(count).png"))
+  count += 1
+  result.append(contentsOf: y3)
+}
+
+try? Gnuplot(xs: result.prefix(1000), result.prefix(1000))
+  .plot(multi: true, index: 0, x: 0, y: 1)
+  .plot(multi: true, index: 1, x: 0, y: 1)(.pngLarge(path: "_test1.png"))
+try? Gnuplot(xs: result.prefix(1000), result.prefix(1000))
+  .plot(multi: true, index: 0, x: 0, y: 1)
+  .plot(multi: true, index: 1, x: 0, y: 1)(.pngLarge(path: "_test2.png"))
+dni.map { $0 < 5 ? 0 : $0 }.forEach { print($0) }
+*/
--- Sources/Playground/main.swift
+++ Sources/Playground/main.swift
@@ -1,113 +1,69 @@
-
 import Foundation
 import Utilities
 
 
-// let c = CSV(atPath: "/workspaces/SPC/Saudian.csv")!
-// let dni = c["DNI"]
-// let ss = dni.chunks(ofCount: 24)
-// var count = 1
-// var result = [Double]()
+// print(cleanUp(formulasCalculation, titlesCalculation).joined(separator: "\n\n"))
+// print(cleanUp(formulasDaily1, titlesDaily1, 365).joined(separator: "\n\n"))
+// print(cleanUp(formulasDaily2, titlesDaily2, 365).joined(separator: "\n\n"))
 
-// for day in ss {
-//   let y1 = Array(day)
-//   let x1 = [Double](stride(from: 0, to: Double(y1.count), by: 1))
-//   let x2 = [Double](stride(from: 0, to: Double(y1.count), by: 1 / 12))
-//   let d = CubicSpline(x: x1, y: y1)
-//   let y2 = x2.map(d.callAsFunction(x:)).map { max(0, $0) }
-//   let factor = y1.reduce(0, +) / (y2.reduce(0, +) / 12)
-//   let y3 = y2.map { factor * $0 }
-//   // try! Gnuplot(xs: x1, x2, x2, ys: y1, y2, y3)(.pngLarge(path: "dni_\(count).png"))
-//   count += 1
-//   result.append(contentsOf: y3)
-// }
+try! cleanUp(formulasCalculation, titlesCalculation).joined(separator: "\n\n").write(toFile: "Calculation.swift", atomically: false, encoding: .utf8)
+try! _ = Process.run(.init(fileURLWithPath: "/workspaces/swift-format/.build/release/swift-format"), arguments: ["-i", "Calculation.swift"], terminationHandler: nil)
+try! cleanUp(formulasDaily1, titlesDaily1, 365).joined(separator: "\n\n").write(toFile: "Daily1.swift", atomically: false, encoding: .utf8)
+try! _ = Process.run(.init(fileURLWithPath: "/workspaces/swift-format/.build/release/swift-format"), arguments: ["-i", "Daily1.swift"], terminationHandler: nil)
+try! cleanUp(formulasDaily2, titlesDaily2, 365).joined(separator: "\n\n").write(toFile: "Daily2.swift", atomically: false, encoding: .utf8)
+try! _ = Process.run(.init(fileURLWithPath: "/workspaces/swift-format/.build/release/swift-format"), arguments: ["-i", "Daily2.swift"], terminationHandler: nil)
 
-// try? Gnuplot(xs: result.prefix(1000), result.prefix(1000))
-//   .plot(multi: true, index: 0, x: 0, y: 1)
-//   .plot(multi: true, index: 1, x: 0, y: 1)(.pngLarge(path: "_test1.png"))
-// try? Gnuplot(xs: result.prefix(1000), result.prefix(1000))
-//   .plot(multi: true, index: 0, x: 0, y: 1)
-//   .plot(multi: true, index: 1, x: 0, y: 1)(.pngLarge(path: "_test2.png"))
-// // dni.map { $0 < 5 ? 0 : $0 }.forEach { print($0) }
+func inputOutput() {
 
-func foo() {
+  let workbook = try! XML(atPath: "/workspaces/SPC/xl/workbook.xml")
+  let definedNamesRef: [(Int, String)] = workbook.children[5].children
+    .map {
+      let ref = $0.value.replacingOccurrences(of: "Input_Output_Summary!", with: "")
+        .replacingOccurrences(of: "$", with: "")
+      return (Int(String(ref.dropFirst()))!, ref)
+    }
 
-let workbook = try! XML(atPath: "/workspaces/SPC/xl/workbook.xml")
-let definedNamesRef: [(Int, String)] = workbook.children[5].children.map {
-  let ref = $0.value.replacingOccurrences(of: "Input_Output_Summary!", with: "").replacingOccurrences(of: "$", with: "") 
-  return (Int(String(ref.dropFirst()))!, ref)
-}
+  let definedNames = workbook.children[5].children.map { $0.attributes["name"]! }
 
-let definedNames = workbook.children[5].children.map { $0.attributes["name"]! }
+  let xml1 = try! XML(atPath: "/workspaces/SPC/xl/worksheets/sheet1.xml")
+  let sheetData1 = xml1.children.first(where: { $0.name == "sheetData" })!
+  let rows1 = sheetData1.children
 
-let xml1 = try! XML(atPath: "/workspaces/SPC/xl/worksheets/sheet1.xml")
-let sheetData1 = xml1.children.first(where: {$0.name == "sheetData"})!
-let rows1 = sheetData1.children
-
-let design = definedNamesRef.indices.compactMap { i -> (String, String)? in 
-  let ref = definedNamesRef[i]
-  let name = definedNames[i]
-  if let formula = rows1.first(where: { $0.attributes["r"]! == String(ref.0)})?
-  .children.first(where: { $0.attributes["r"]! == ref.1})?
-  .children.first(where: { att in att.name == "f" })?
-  .value {
-    return (name, formula)
-  } else if let value = rows1.first(where: { $0.attributes["r"]! == String(ref.0)})?
-  .children.first(where: { $0.attributes["r"]! == ref.1})?
-  .children.first(where: { att in att.name == "v" })?
-  .value {
-    return (name, value)
+  let design = definedNamesRef.indices.compactMap { i -> (String, String)? in
+    let ref = definedNamesRef[i]
+    let name = definedNames[i]
+    if let formula = rows1.first(where: { $0.attributes["r"]! == String(ref.0) })?.children
+      .first(where: { $0.attributes["r"]! == ref.1 })?
+      .children.first(where: { att in att.name == "f" })?
+      .value
+    {
+      return (name, formula)
+    } else if let value = rows1.first(where: { $0.attributes["r"]! == String(ref.0) })?.children
+      .first(where: { $0.attributes["r"]! == ref.1 })?
+      .children.first(where: { att in att.name == "v" })?
+      .value
+    {
+      return (name, value)
+    }
+    return nil
   }
-  return nil
-}.map { "let \($0.0): Double" }
-print(design.joined(separator: "\n"))
- /*
-print(design.joined(separator: "\n").replacingOccurrences(of: "MAX(", with: "max(")
-    .replacingOccurrences(of: "MIN(", with: "min(")
+  let declaration = design.map { "let \($0.0): Double" }.joined(separator: "\n")
+  let assign = design.map { "self.\($0.0) = \($0.1)" }.joined(separator: "\n")
+    .replacingOccurrences(of: "MAX(", with: "max(").replacingOccurrences(of: "MIN(", with: "min(")
     .replacingOccurrences(of: "IFERROR(", with: "ifFinite(\n ")
     .replacingOccurrences(of: "IF(", with: "iff(\n ")
     .replacingOccurrences(of: "AND(", with: "and(\n ")
-    .replacingOccurrences(of: "OR(", with: "or(\n ")
-    .replacingOccurrences(of: ",", with: ",\n ")
-    .replacingOccurrences(of: ":", with: ",")
-    .replacingOccurrences(of: "^", with: "**")
-    .replacingOccurrences(of: "\"", with: ""))
+    .replacingOccurrences(of: "OR(", with: "or(\n ").replacingOccurrences(of: ",", with: ",\n ")
+    .replacingOccurrences(of: ":", with: ",").replacingOccurrences(of: "^", with: "**")
+    .replacingOccurrences(of: "\"", with: "")
 
-
- 
-let sharedStrings = try! XML(atPath: "/workspaces/SPC/xl/sharedStrings.xml")
-let strings = sharedStrings.children.compactMap { $0.children.first?.value }
-
-do {
-let xml = try! XML(atPath: "/workspaces/SPC/xl/worksheets/sheet7.xml")
-let start = 1
-
-
-let sheetData = xml.children.first(where: {$0.name == "sheetData"})!
-let rows = Array(sheetData.children.prefix(100))
-let indices = rows.first!.children.indices
-let header = Dictionary(uniqueKeysWithValues: indices.compactMap { i -> (String, String)? in 
-if let key = rows[3].children[i].attributes["r"], let value = 
-rows[3].children[i].children.first(where: { att in att.name == "v" })?.value {
-  return ("\(key.filter(\.isLetter))", strings[Int(value)!])
-} else {
-  return nil
+  let sharedStrings = try! XML(atPath: "/workspaces/SPC/xl/sharedStrings.xml")
+  let strings = sharedStrings.children.compactMap { $0.children.first?.value }
 }
-})
-let dict = Dictionary(uniqueKeysWithValues: indices.compactMap { i -> (String, String)? in 
-  if let index = rows.firstIndex(where: { node in 
-    if node.children.endIndex <= i { return false }
-    return !(node.children[i].children.first(where: { att in att.name == "f" })?.value.isEmpty ?? true) }
-  ), let key = rows[index].children[i].attributes["r"] {
-    if key == "IP" { return nil }
-    let letters = "\(key.filter(\.isLetter))"
-    var formula = rows[index].children[i].children.first(where: { att in att.name == "f" })!.value
-    formula = formula.replacingOccurrences(of: "8763", with: "_end")
-    .replacingOccurrences(of: "\(index+1)", with: "_[i]")
-    .replacingOccurrences(of: "\(index)", with: "_[i-1]")
-    .replacingOccurrences(of: "\(index+1)", with: "_[i+1]")
-    formula = formula.replacingOccurrences(of: "$", with: "")
-    .replacingOccurrences(of: "=", with: "==")
+/*
+var cleanFormulas = [String:String]()
+for formula in calculationFormulas {
+  var expression = formula.value.dropFirst()
     .replacingOccurrences(of: "MAX(", with: "max(")
     .replacingOccurrences(of: "MIN(", with: "min(")
     .replacingOccurrences(of: "IFERROR(", with: "ifFinite(\n ")
@@ -115,49 +71,35 @@ let dict = Dictionary(uniqueKeysWithValues: indices.compactMap { i -> (String, S
     .replacingOccurrences(of: "AND(", with: "and(\n ")
     .replacingOccurrences(of: "OR(", with: "or(\n ")
     .replacingOccurrences(of: "COUNT", with: "count")
-    formula = formula.replacingOccurrences(of: ",", with: ",\n ")
-    .replacingOccurrences(of: ":", with: ",")
+  expression = expression.replacingOccurrences(of: "=", with: "==")
+    .replacingOccurrences(of: "#", with: "_")
+    .replacingOccurrences(of: "$", with: "")
+    .replacingOccurrences(of: "&", with: "")
+    .replacingOccurrences(of: "!", with: "_")
+    .replacingOccurrences(of: ":", with: "...")
     .replacingOccurrences(of: "^", with: "**")
     .replacingOccurrences(of: "\"", with: "")
-    return (letters, formula)
- 
-  } else {
-    return nil
+  columns.reversed().forEach {
+    expression = expression.replacingOccurrences(of: "\($0)3", with: "xy[\($0) + i]")
   }
-})
-
-let A = UnicodeScalar("A").value
-let count = dict.keys.count
-precondition(count < 676)
-
-let columns = (0..<count).map { n -> (Int,String) in
-  let i = n.quotientAndRemainder(dividingBy: 26)
-  let q = i.quotient > 0 ? String(UnicodeScalar(A + UInt32(i.quotient-1))!) : ""
-  return (n, q + String(UnicodeScalar(A + UInt32(i.remainder))!))
+  cleanFormulas[formula.key] = expression
 }
-
-
-
-for key in columns {
-  guard var formula = dict[key.1] else { continue }
-  for key2 in columns.reversed() {
-    formula = formula.replacingOccurrences(of: key2.1 + "_[", with: "xy[\(key2.1)+")
+var i = 0
+let text: [String] = columns.compactMap { 
+  if i == 440 {
+    print(i)
   }
-  print("""
-  /// \(header[key.1] ?? "")
-  let \(key.1) = \((key.0+4)*8760)
-  let \(key.1)0 = \((key.0-4)*365)
+  guard let formula = cleanFormulas[$0] else { return nil }
+  i += 1
+  return """
+  let \($0) = \((i-1)*8760+cleanFormulas.keys.count*365)
+  let \($0)0 = \((i-1)*365)
   for i in 1..<8760 {
-    xy[\(key.1)+i] = \(formula)
-    xy[\(key.1)0 + day[i]] += xy[\(key.1) + i]
-    c[\(key.1)0 + day[i]] += xy[\(key.1) + i] > 0 ? 1 : 0
+    xy[\($0)+i] = \(formula)
+    xy[\($0)0 + day[i]] += xy[\($0) + i]
+    c[\($0)0 + day[i]] += xy[\($0) + i] > 0 ? 1 : 0
   }
-
-  """)
+  """
 }
-
-}
+print(text.joined(separator: "\n"))
 */
-}
-
-foo()
