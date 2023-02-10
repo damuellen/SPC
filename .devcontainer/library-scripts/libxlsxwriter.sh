9855950e13e6fe030d2b45ebf12aed16b0f07e6a
Daniel <damuellen@aol.com>
2023-02-10 12:30:20 +0100

    DTOA

--- .devcontainer/library-scripts/libxlsxwriter.sh
+++ .devcontainer/library-scripts/libxlsxwriter.sh
@@ -5,7 +5,7 @@ echo "Install libxlsxwriter"
 cd /tmp
 git clone --single-branch -b RELEASE_1.1.4 https://github.com/jmcnamara/libxlsxwriter --quiet
 cd libxlsxwriter
-make
+make USE_DTOA_LIBRARY=1 USE_MEM_FILE
 sudo make install
 sudo ldconfig
 cd ../
