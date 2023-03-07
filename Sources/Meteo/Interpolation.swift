e058a0858d327dd2f6be2ec31b35d89542bab548
Daniel Müllenborn <damuellen@aol.com>
2023-03-07 20:02:07 +0100

    Update Interpolation.swift

--- Sources/Meteo/Interpolation.swift
+++ Sources/Meteo/Interpolation.swift
@@ -11,13 +11,13 @@
 extension Array where Element == MeteoData {
   /// Interpolation function for meteo data
   internal func interpolate(steps: Int) -> [MeteoData] {
-    if data.count < 2 { return data }
+    if self.count < 2 { return self }
     let (temperature, dni, ghi, dhi, windSpeed) =
-      data.map(\.temperature).interpolate(steps: steps),
-      data.map(\.dni).interpolate(steps: steps),
-      data.map(\.ghi).interpolate(steps: steps),
-      data.map(\.dhi).interpolate(steps: steps),
-      data.map(\.windSpeed).interpolate(steps: steps))
+      self.map(\.temperature).interpolate(steps: steps),
+      self.map(\.dni).interpolate(steps: steps),
+      self.map(\.ghi).interpolate(steps: steps),
+      self.map(\.dhi).interpolate(steps: steps),
+      self.map(\.windSpeed).interpolate(steps: steps))
     return data.indices.map { i -> MeteoData in
       MeteoData(dni: dni[i], ghi: ghi[i], dhi: dhi[i],
         temperature: temperature[i], windSpeed: windSpeed[i])
