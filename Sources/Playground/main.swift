64544df67b229367ef51ef72032f58b1a29226a5
Daniel <damuellen@aol.com>
2023-03-07 15:26:03 +0100

    Win

--- Sources/Playground/main.swift
+++ Sources/Playground/main.swift
@@ -1,24 +1,46 @@
 import Foundation
 import Utilities
 
+let path: String
+#if os(Windows)
+path = FileDialog()
+#else
+path = CommandLine.arguments[2]
+#endif
+
 let newLine = UInt8(ascii: "\n")
 let cr = UInt8(ascii: "\r")
 let comma = UInt8(ascii: ",")
 let point = UInt8(ascii: ".")
 let separator = UInt8(ascii: ";")
 
-let data = try! Data(contentsOf: URL(fileURLWithPath: CommandLine.arguments[2]))
+let steps = Int(CommandLine.arguments[1])!
+let data = try! Data(contentsOf: URL(fileURLWithPath: path))
 var lines = data.filter({ $0 != cr }).split(separator: newLine, maxSplits: 13, omittingEmptySubsequences: false)
 let csv = CSVReader(data: Data(lines[10] + [newLine] + lines[13].map { $0 == comma ? point : $0 }), separator: ";")
-
-print(csv!.headerRow!.joined(separator: ","), separator: "")
-var steps = Int(CommandLine.arguments[1])!
 
 var buffer = [[Double]]()
 for column in csv!.dataRows.transposed() {
   buffer.append(column.interpolate(steps: steps))
 }
 
-for row in buffer.transposed() {
-  print(row.map(\.description).joined(separator: ","), separator: ",")
+#if os(Windows)
+let url = URL(fileURLWithPath: path)
+let fileURL = url.deletingLastPathComponent().appendingPathComponent(pathComponent: "\(steps)" + url.lastPathComponent)
+#else
+let fileURL = URL(fileURLWithPath: CommandLine.arguments[3])
+#endif
+
+do {
+    try Data((csv!.headerRow!.joined(separator: ",") + "\n").utf8).write(to: fileURL)
+    let fileHandle = try FileHandle(forWritingTo: fileURL)
+    fileHandle.seekToEndOfFile()
+    for row in buffer.transposed() {
+      fileHandle.write(Data((row.map(\.description).joined(separator: ",")  + "\n").utf8))
+    }
+    fileHandle.closeFile()
+    print(fileURL.absoluteString)
+} catch {
+    print(error)
 }
+
