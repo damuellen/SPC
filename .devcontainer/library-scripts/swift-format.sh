9e00997e95d574f21ee0db3030d0556a8c575554
Daniel Müllenborn <damuellen@aol.com>
2023-05-25 22:02:58 +0200

    Update swift-format.sh

--- .devcontainer/library-scripts/swift-format.sh
+++ .devcontainer/library-scripts/swift-format.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Install swift-format"
 cd /tmp
-git clone --single-branch -b 0.50700.1 https://github.com/apple/swift-format.git --quiet
+git clone --single-branch -b 508.0.1 https://github.com/apple/swift-format.git --quiet
 cd swift-format
 swift build -c release --product swift-format
 cp .build/release/swift-format /usr/local/bin/swift-format
