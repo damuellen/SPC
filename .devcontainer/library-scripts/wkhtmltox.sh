fad54fd95d54474aa9b16aa30994908bcc8969fd
Daniel Müllenborn <damuellen@aol.com>
2023-05-25 16:54:03 +0000

    I

--- .devcontainer/library-scripts/wkhtmltox.sh
+++ .devcontainer/library-scripts/wkhtmltox.sh
@@ -2,10 +2,9 @@
 echo "Install wkhtmltopdf"
 cd /tmp
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
-export DEBIAN_FRONTEND=noninteractive
-sudo apt install -f /tmp/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
+sudo apt install -y -f /tmp/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
 sudo apt update
-sudo apt install xfonts-base xfonts-75dpi
-sudo apt install -f /tmp/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
+sudo apt install -y xfonts-base xfonts-75dpi
+sudo apt install -y -f /tmp/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
 cd ../
 rm -rf /tmp/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
