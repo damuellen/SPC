e30fa44b9f08e8dbd7a85db2e1afc5490de621fd
Daniel <damuellen@aol.com>
2023-06-15 23:20:28 +0200

    Availability

--- Sources/BlackBoxModel/Availability.swift
+++ Sources/BlackBoxModel/Availability.swift
@@ -13,45 +13,38 @@ import Utilities
 
 struct Availability: Codable {
 
-  static var current = withDefaults()
+  static var current = Availability()
 
   static var fuel: Double = 5
 
-  private var index: Int { DateTime.current.month }
+  private var index: Int { DateTime.current.month + 1 }
 
   private let data: [Values]
-
-  private static let defaults: Availability.Values = .init(
-    solarField: 0.993, breakHCE: 0.001, airHCE: 0.0, fluorHCE: 0.0,
-    reflMirror: 0.93, missgMirror: 0.0005, powerBlock: 1.0, storage: 1.0
-  )
 
   init(_ data: [Values]) { self.data = data }
 
   public struct Values: Codable {
-    var solarField: Ratio
-    var breakHCE: Ratio
-    var airHCE: Ratio
-    var fluorHCE: Ratio
-    var reflMirror: Ratio
-    var missgMirror: Ratio
-    var powerBlock: Ratio
-    var storage: Ratio
+    var solarField: Ratio = 0.993
+    var breakHCE: Ratio = 0.001
+    var airHCE: Ratio = 0.0
+    var fluorHCE: Ratio = 0.0
+    var reflMirror: Ratio = 0.93
+    var missgMirror: Ratio = 0.0005
+    var powerBlock: Ratio = 1.0
+    var storage: Ratio = 1.0
   }
 
   var value: Values { self.data[index] }
 
   var values: Values { self.data[0] }
 
-  static func withDefaults() -> Availability {
-    Availability(Array(repeating: Availability.defaults, count: 13))
-  }
+  private init() { self.data = Array(repeating: Values(), count: 13) }
 }
 
 extension Availability {
   public init(file: TextConfigFile) throws {
     let ln: (Int) throws -> Double = { try file.readDouble(lineNumber: $0) }
-    var data = [Availability.defaults]
+    var data = [Values()]
     for n in 0..<12 {
       let offset = 3 * n
       try data.append(
