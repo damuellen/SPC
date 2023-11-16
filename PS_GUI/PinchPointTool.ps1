687cb64c6c3a08d254739de1ff8ccb5513ade520
Daniel <damuellen@aol.com>
2023-11-16 20:36:37 +0100

    PP PS

--- PS_GUI/PinchPointTool.ps1
+++ PS_GUI/PinchPointTool.ps1
@@ -1,6 +1,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 $global:Expert = $false
+$executableName = "PinchPointTool.exe"
 $WidthBox = 60
 $HeightBox = 24
 $XLabels = 10
@@ -159,18 +160,22 @@ $Button3.Add_Click({
 
 Function New-Process {
     $P = new-object System.Diagnostics.Process
-    $P.StartInfo.Filename = "C:\bin\PinchPointTool.exe"
+    $P.StartInfo.Filename = $executableName
     $P.StartInfo.RedirectStandardOutput = $true;
     $P.StartInfo.RedirectStandardInput = $true;
     $P.StartInfo.UseShellExecute = $false;
+    $P.StartInfo.CreateNoWindow = $true
     return $P;
 }
 
 Function Read-Inputs {
     $P = New-Process;
     $P.StartInfo.Arguments = "-json", "-case 1"
-    $P.start()
+    if (-not $P.Start()) {
+        return $false;
+    }
     $text = $P.StandardOutput.ReadToEnd()
+    $P.WaitForExit()
     $start = $text.IndexOf("{")
     $length = $text.LastIndexOf("}") - $start + 1
     $text = $text.Substring($start, $length)
@@ -193,6 +198,7 @@ Function Read-Inputs {
         $_.Text = $Array[$idx]
         $idx = $idx + 1
     })
+    return $true;
 }
 
 Function Read-Inputs2 {
@@ -293,34 +299,37 @@ Function Run-PDF {
         $Arguments += "-pdf"
         $Arguments = $Arguments.Trim()
         $P.StartInfo.Arguments = $Arguments
+        $P.StartInfo.WorkingDirectory = $env:Temp
         $P.start()
         $P.WaitForExit()
         write-host $P.TotalProcessorTime
-        Start-Process ((Resolve-Path ".\diagram.pdf").Path)
-        Start-Process ((Resolve-Path ".\plot.pdf").Path)
+        Start-Process ((Join-Path $env:Temp ".\diagram.pdf").Path)
+        Start-Process ((Join-Path $env:Temp ".\plot.pdf").Path)
     } else {
         $oReturn=[System.Windows.Forms.Messagebox]::Show("Invalid input")
     }
 }
 
 Function Load-Inputs {
-    $Array = Get-Content -Path $env:Temp\pinchpoint.ini
-    $idx = 0
-    if ($Array.Length -ge 11) {
-        $Form.Controls.where{$_ -is [System.Windows.Forms.TextBox]}.ForEach({
+    $iniFilePath = Join-Path $env:Temp 'pp.ini'
+    if (Test-Path -Path $iniFilePath -PathType Leaf) {
+        $Array = Get-Content -Path $iniFilePath
+        $idx = 0
+        if ($Array.Length -ge 11) {
+            $Form.Controls.where{$_ -is [System.Windows.Forms.TextBox]}.ForEach({
+                $_.Text = $Array[$idx]
+                $idx = $idx + 1
+            })
+        } else {
+            Remove-Item $iniFilePath
+        }
+        if ($Array.Length -eq 21) {
+        $global:Expert = $true
+        $Form1.Controls.where{$_ -is [System.Windows.Forms.TextBox]}.ForEach({
             $_.Text = $Array[$idx]
             $idx = $idx + 1
         })
-    } else {
-        Remove-Item $env:Temp\pinchpoint.ini
-        Read-Inputs
-    }
-    if ($Array.Length -eq 21) {
-    $global:Expert = $true
-    $Form1.Controls.where{$_ -is [System.Windows.Forms.TextBox]}.ForEach({
-        $_.Text = $Array[$idx]
-        $idx = $idx + 1
-    })
+        }
     }
 }
 
@@ -336,11 +345,38 @@ Function Save-Inputs {
             $Array += $_.Text
         }
     })
+    $iniFilePath = Join-Path $env:Temp 'pp.ini'
     if ($Array.Length -eq 11 -or $Array.Length -eq 21) {
-        Set-Content -Path $env:Temp\pinchpoint.ini -Value $Array
+        Set-Content -Path $iniFilePath -Value $Array
     }
 }
 
+# Function to recursively search for the executable file
+function SearchForExecutable {
+    param (
+        [string]$folder,
+        [string]$executable
+    )
+    $firstPathFound = $null
+    $userPath = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
+    Get-ChildItem -Path $folder -Recurse -Filter $executable | ForEach-Object {
+        if (-not $firstPathFound) {
+            $folderContainingExecutable = $_.FullName.Replace($executable, "")
+            if ($userPath -notlike "*$folderContainingExecutable;*") {
+                $firstPathFound = $folderContainingExecutable
+                [System.Environment]::SetEnvironmentVariable("PATH", "$firstPathFound;$userPath", [System.EnvironmentVariableTarget]::User)
+            } 
+        }
+    }
+    return $firstPathFound
+}
+
+if (-not Read-Inputs) {
+    $found = SearchForExecutable -folder Get-Location -executable $executableName
+    if ($found) { Read-Inputs }
+}
 Load-Inputs
+SearchForExecutable -folder Get-Location -executable "wkhtmltopdf.exe"
+SearchForExecutable -folder Get-Location -executable "gnuplot.exe"
 
 [void]$Form.ShowDialog()
